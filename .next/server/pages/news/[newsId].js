35db5b15503df788a533f4589c491c459ed5f093
arturyyys <arturyyys@gmail.com>
2022-02-10 20:50:52 +0200

    fix

---
+++ .next/server/pages/news/[newsId].js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/news/[newsId]";
+exports.ids = ["pages/news/[newsId]"];
+exports.modules = {
+
+/***/ "./pages/news/[newsId].js":
+/*!********************************!*\
+  !*** ./pages/news/[newsId].js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\n// our-domain.com/news/something-important\nfunction DetailPage() {\n    return(/*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: \"The Detail Page\"\n    }, void 0, false, {\n        fileName: \"C:\\\\Users\\\\artur\\\\OneDrive\\\\Desktop\\\\react\\\\react-complete-guide\\\\pages\\\\news\\\\[newsId].js\",\n        lineNumber: 4,\n        columnNumber: 10\n    }, this));\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (DetailPage);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9uZXdzL1tuZXdzSWRdLmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQSxFQUEwQztTQUVqQ0EsVUFBVSxHQUFHLENBQUM7SUFDckIsTUFBTSw2RUFBRUMsQ0FBRTtrQkFBQyxDQUFlOzs7Ozs7QUFDNUIsQ0FBQztBQUVELGlFQUFlRCxVQUFVLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9uZXh0anMtY291cnNlLy4vcGFnZXMvbmV3cy9bbmV3c0lkXS5qcz80Y2JkIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIG91ci1kb21haW4uY29tL25ld3Mvc29tZXRoaW5nLWltcG9ydGFudFxyXG5cclxuZnVuY3Rpb24gRGV0YWlsUGFnZSgpIHtcclxuICByZXR1cm4gPGgxPlRoZSBEZXRhaWwgUGFnZTwvaDE+O1xyXG59XHJcblxyXG5leHBvcnQgZGVmYXVsdCBEZXRhaWxQYWdlO1xyXG4iXSwibmFtZXMiOlsiRGV0YWlsUGFnZSIsImgxIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/news/[newsId].js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/news/[newsId].js"));
+module.exports = __webpack_exports__;
+
+})();

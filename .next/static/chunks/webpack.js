047821b2fb66a74fcd495d83e7d42a4e5eeb2ae1
arturyyys <arturyyys@gmail.com>
2022-02-13 20:19:43 +0200

    The _app.js File & Layout Wrapper

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2adfba0ad7116eef"; }
+/******/ 		__webpack_require__.h = function() { return "e1880032f46478c0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

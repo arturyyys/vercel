702af6bd473bb1200e50c96a7a2e3692680c4e7c
arturyyys <arturyyys@gmail.com>
2022-02-13 21:30:12 +0200

    Using Programmatic (Imperative) Navigation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e1880032f46478c0"; }
+/******/ 		__webpack_require__.h = function() { return "eae17882ac66de3c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

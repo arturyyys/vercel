166cb45a4103d8e7b99291959031dc4d8d6d7b44
arturyyys <arturyyys@gmail.com>
2022-02-15 20:46:13 +0200

    Working with Params for SSG Data Fetching

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "008e8bc69ad0edf6"; }
+/******/ 		__webpack_require__.h = function() { return "b8df64361f06cd44"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

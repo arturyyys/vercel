a530529c152d89e028f9a825a7cbc961a8c5811f
arturyyys <arturyyys@gmail.com>
2022-02-19 20:39:45 +0200

    Adding Head Metadata

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "efa8f57b6aa3da39"; }
+/******/ 		__webpack_require__.h = function() { return "8adae8e3089f445d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
